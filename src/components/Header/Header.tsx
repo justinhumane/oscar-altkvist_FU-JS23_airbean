e539212cce858ad9ba0b017e3820b526fdd61157
Oscar Altkvist <oscar.altkvist@gmail.com>
2024-03-03 23:16:47 +0100

    WIP: add navigation.

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,17 +1,37 @@
-import menuClosed from "../../assets/menu-icon.svg";
+import menuOpen from "../../assets/menu-icon.svg";
+import menuClose from "../../assets/menu-close-icon.svg";
 import cart from "../../assets/cart-icon.svg";
 import "./Header.scss";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const HeaderComp = () => {
+  const [menuToggle, setMenuToggle] = useState(false);
+
   return (
-    <div className="header">
-      <div className="menu-icon">
-        <img src={menuClosed} alt="" />
+    <>
+      <div className="header">
+        <header>
+          <div className="menu-icon" onClick={() => setMenuToggle(!menuToggle)}>
+            <img src={menuOpen} alt="" />
+          </div>
+          <div className="cart-icon">
+            <img src={cart} alt="" />
+          </div>
+        </header>
+        <div className={"mobile-menu" + (menuToggle ? "" : " hidden")}>
+          <div className="menu-icon" onClick={() => setMenuToggle(!menuToggle)}>
+            <img src={menuClose} alt="" />
+          </div>
+          <nav className="nav">
+            <Link to="/menu">Meny</Link>
+            <Link to="/about">Vårt kaffe</Link>
+            <Link to="/profile">Min profil</Link>
+            <Link to="/status">Orderstatus</Link>
+          </nav>
+        </div>
       </div>
-      <div className="cart-icon">
-        <img src={cart} alt="" />
-      </div>
-    </div>
+    </>
   );
 };
 
