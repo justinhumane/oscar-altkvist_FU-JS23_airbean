57871aa88b7f3ee1ae8f96ccb4fd1ca0b72d03e8
Oscar Altkvist <oscar.altkvist@gmail.com>
2024-03-03 16:19:28 +0100

    Style landing page.

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,5 +1,19 @@
+import "./Landing.scss";
+import logo from "../assets/logo.png";
+import { useNavigate } from "react-router-dom";
+
 const LandingPage = () => {
-  return <h1>Landing Page</h1>;
+  const navigate = useNavigate();
+
+  return (
+    <div className="landing-page" onClick={() => navigate("/menu")}>
+      <div className="logo">
+        <img src={logo} alt="" />
+      </div>
+      <h1>Air Bean</h1>
+      <p>Dronedelivered coffee</p>
+    </div>
+  );
 };
 
 export default LandingPage;
