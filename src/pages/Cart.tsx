7700c8ae3b76cf401f29dbf20ffa9080db0dbbcd
Oscar Altkvist <oscar.altkvist@gmail.com>
2024-03-13 10:03:40 +0100

    Remove unnecessary files

--- src/pages/Cart.tsx
+++
@@ -1,5 +0,0 @@
-const CartPage = () => {
-  return <h1>Cart</h1>;
-};
-
-export default CartPage;
--- src/pages/Status.tsx
+++
@@ -1,5 +0,0 @@
-const StatusPage = () => {
-  return <h1>Status</h1>;
-};
-
-export default StatusPage;
